a0cc81a763d8bbd5a0b17d35dc7e4fc401b4218f
MeghanaKancherla <meghana.kancherla2911@gmail.com>
2021-04-27 14:28:04 +0530

    interface added

--- app/src/main/java/com/example/adapp/presenter/RegisterPresenter.kt
+++ app/src/main/java/com/example/adapp/presenter/RegisterPresenter.kt
@@ -1,32 +1,35 @@
 package com.example.adapp.presenter
 
-import android.app.Activity
-import android.app.Application
-import android.content.Context
 import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
 import com.example.adapp.model.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 
-class RegisterPresenter(val activity: FragmentActivity){
-     fun createAccount(username: String, email: String, password: String, phoneNo: String) {
+class RegisterPresenter(val view: View){
+
+    fun createAccount(username: String, email: String, password: String, phoneNo: String) {
         val fAuth= FirebaseAuth.getInstance()
         fAuth.createUserWithEmailAndPassword(email,password)
-            .addOnCompleteListener(activity){
-                    task ->
+            .addOnCompleteListener{ task ->
                 if(task.isSuccessful)
                 {
                     val user=fAuth.currentUser
                     val userObj= User(username,email,password,phoneNo)
                     FirebaseDatabase.getInstance().getReference("Users")
                         .child(user.uid).setValue(userObj)
+                    view.sendToast("Registration is successful")
+
                 }
                 else
                 {
-                    Toast.makeText(activity,"Unable to complete Registration..", Toast.LENGTH_SHORT).show()
+                    view.sendToast("Unable to complete Registration..")
                 }
 
             }
+    }
+
+    interface View{
+        fun sendToast(message: String)
     }
 }
