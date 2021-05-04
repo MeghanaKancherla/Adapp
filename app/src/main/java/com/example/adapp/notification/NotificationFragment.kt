b5b964c1a397d5bcd49e07597efbce01cebf91ad
MeghanaKancherla <meghana.kancherla2911@gmail.com>
2021-05-04 15:02:12 +0530

    notification

--- app/src/main/java/com/example/adapp/notification/NotificationFragment.kt
+++ app/src/main/java/com/example/adapp/notification/NotificationFragment.kt
@@ -2,6 +2,7 @@ package com.example.adapp.notification
 
 import android.os.Bundle
 import android.os.TokenWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -80,6 +81,7 @@ class NotificationFragment : Fragment(), NotificationDisplayPresenter.View, Retr
     }
 
     override fun onResponse(response: Ad_response) {
+        Log.d("NotificationFragment", "${response.listOfAds}")
         rView.adapter = NotificationRecyclerViewAdapter(response.listOfAds!!){
             val adDisplay = AddDetailsFragment.newInstance(it)
             activity?.supportFragmentManager?.beginTransaction()
